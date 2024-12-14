c7f8d4cc0b9181280e93da76852c1f03eda99ab1
ndorfin <ndorfin@users.noreply.github.com>
2024-12-14 13:48:45 +0000

    deploy: 21a6c002ac69367eff973f8836d69b26cdda6c92

---
+++ assets/mjs/wc/map-embed.mjs
@@ -0,0 +1,86 @@
+class MapEmbed extends HTMLElement {
+
+	#map;
+	#tiles;
+	#markers;
+
+	#checkSourceElementExists(elemId) {
+		if (document.getElementById(elemId)) return true;
+		return false;
+	}
+
+	#addScript() {
+		const attributes = {
+			id: 'script-leaflet',
+			url: 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+			integrity: 'sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=',
+			crossorigin: '',
+		}
+		if (!this.#checkSourceElementExists(attributes.id)) {
+			let scriptElem = document.createElement('script');
+			scriptElem.id = attributes.id;
+			scriptElem.src = attributes.url;
+			scriptElem.integrity = attributes.integrity;
+			scriptElem.crossOrigin = attributes.crossorigin;
+			scriptElem.onload = this.#invokeLeaflet.bind(this);
+			document.head.append(scriptElem);
+		}
+	}
+
+	#addCSS() {
+		const attributes = {
+			id: 'stylesheet-leaflet',
+			url: 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+			integrity: 'sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=',
+			crossorigin: '',
+		}
+		if (!this.#checkSourceElementExists(attributes.id)) {
+			let linkElem = document.createElement('link');
+			linkElem.id = attributes.id;
+			linkElem.rel = 'stylesheet';
+			linkElem.href = attributes.url;
+			linkElem.integrity = attributes.integrity;
+			linkElem.crossOrigin = attributes.crossorigin;
+			document.head.append(linkElem);
+		}
+	}
+
+	#addMapPointsFromJSON() {
+		const JSON = this.getAttribute('json');
+		fetch(JSON).then(response => response.json()).then(data => {
+			let markerArray = [];
+			data.forEach(item => {
+				let id = item.id;
+				let markerObj = {
+					id,
+					location: {
+						latitude: item.location.latitude || item.location.area.location.latitude,
+						longitude: item.location.longitude || item.location.area.location.longitude,
+					}
+				};
+				markerArray.push(markerObj);
+				L.marker([markerObj.location.latitude, markerObj.location.longitude]).addTo(this.#map);
+			});
+			this.#markers = markerArray;
+		});
+	}
+
+	#invokeLeaflet() {
+		this.#map = window.L.map(this.getAttribute('id')).setView([-33.931888, 18.716808], 14);
+		this.#tiles = window.L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			maxZoom: 19,
+			attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+		}).addTo(this.#map);
+
+		if (this.getAttribute('json')) {
+			this.#addMapPointsFromJSON();
+		}
+	}
+
+	connectedCallback() {
+		this.#addScript();
+		this.#addCSS();
+	}
+}
+
+customElements.define('map-embed', MapEmbed);
