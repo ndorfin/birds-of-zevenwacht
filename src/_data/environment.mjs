eb9dc15ee2bff3e7aeacc6376609b335d2266d0a
Shaun O'Connell <ndorfin@gmail.com>
2024-12-19 15:30:16 +0200

    Self-host leaflet

--- src/_data/environment.mjs
+++ src/_data/environment.mjs
@@ -2,7 +2,7 @@ export default function () {
 	return {
 		name: process.env.ENVIRONMENT || 'development',
 		baseURL: process.env.BASE_URL || 'https://ndorfin.github.io/birds-of-zevenwacht/',
-		timestampCSSBuild: 1734612905732,
-		timestampMJSBuild: 1734613824065,
+		timestampCSSBuild: 1734614998677,
+		timestampMJSBuild: 1734614971640,
 	};
 }
--- src/assets/mjs/wc/map-embed.mjs
+++ src/assets/mjs/wc/map-embed.mjs
@@ -1,5 +1,5 @@
 function getBaseURI() {
-	if (window.location.href.indexOf('/birds-of-zevenwacht/') > -1) {
+	if (location.href.indexOf('/birds-of-zevenwacht/') > -1) {
 		return '/birds-of-zevenwacht/';
 	} else {
 		return '/'
@@ -18,36 +18,14 @@ class MapEmbed extends HTMLElement {
 	#addScript() {
 		const attributes = {
 			id: 'script-leaflet',
-			url: 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
-			integrity: 'sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=',
-			crossorigin: '',
+			url: '/assets/mjs/lib/leaflet.1.9.4.js',
 		}
 		if (!this.#checkSourceElementExists(attributes.id)) {
 			let scriptElem = document.createElement('script');
 			scriptElem.id = attributes.id;
 			scriptElem.src = attributes.url;
-			scriptElem.integrity = attributes.integrity;
-			scriptElem.crossOrigin = attributes.crossorigin;
 			scriptElem.onload = this.#invokeLeaflet.bind(this);
 			document.head.append(scriptElem);
-		}
-	}
-
-	#addCSS() {
-		const attributes = {
-			id: 'stylesheet-leaflet',
-			url: 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
-			integrity: 'sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=',
-			crossorigin: '',
-		}
-		if (!this.#checkSourceElementExists(attributes.id)) {
-			let linkElem = document.createElement('link');
-			linkElem.id = attributes.id;
-			linkElem.rel = 'stylesheet';
-			linkElem.href = attributes.url;
-			linkElem.integrity = attributes.integrity;
-			linkElem.crossOrigin = attributes.crossorigin;
-			document.head.append(linkElem);
 		}
 	}
 
@@ -59,7 +37,7 @@ class MapEmbed extends HTMLElement {
 			popupAnchor: [0, -36],
 			html: `<span></span>`
 		}
-		return window.L.divIcon(iconObj);
+		return L.divIcon(iconObj);
 	}
 
 	#addMarkerToMap(type, markerObj) {
@@ -82,14 +60,14 @@ class MapEmbed extends HTMLElement {
 				<a href="${ getBaseURI() }areas/${ markerObj.id }/">${ markerObj.area.name }</a>
 			`;
 		}
-		window.L.marker(
+		L.marker(
 			[markerObj.location.latitude, markerObj.location.longitude],
 			{icon: this.#createIcon(type)}
 		).addTo(this.#map).bindPopup(getHTMLByType(type));
 	}
 
 	#addSinglePointMarker(locationObj, type) {
-		let singleMarker = window.L.marker(
+		L.marker(
 			[locationObj.latitude, locationObj.longitude],
 			{icon: this.#createIcon(type)}
 		).addTo(this.#map);
@@ -126,19 +104,19 @@ class MapEmbed extends HTMLElement {
 	}
 
 	#addBoundaries(data) {
-		let polygon = window.L.polygon(data, {color: 'hsla(220 50% 50% / 0.80)'}).addTo(this.#map);
+		let polygon = L.polygon(data, {color: 'hsla(220 50% 50% / 0.80)'}).addTo(this.#map);
 		this.#map.fitBounds(polygon.getBounds());
 	}
 
 	#invokeLeaflet() {
-		this.#map = window.L.map(this.getAttribute('id'), {
+		this.#map = L.map(this.getAttribute('id'), {
 			scrollWheelZoom: false,
 		}).setView(
 			[parseFloat(this.getAttribute('latitude')),parseFloat(this.getAttribute('longitude')),],
 			parseInt(this.getAttribute('zoom'))
 		);
 
-		window.L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+		L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 			attribution: `<a href="${ getBaseURI() }attribution/">Attribution</a>`
 		}).addTo(this.#map);
 
@@ -174,7 +152,6 @@ class MapEmbed extends HTMLElement {
 
 	connectedCallback() {
 		this.#addScript();
-		this.#addCSS();
 	}
 }
 
