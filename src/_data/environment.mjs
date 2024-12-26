faa21c5698d9045815ad1e1afbe961683b44c771
Shaun O'Connell <ndorfin@gmail.com>
2024-12-26 17:01:08 +0200

    add bird card pattern

--- src/_data/environment.mjs
+++ src/_data/environment.mjs
@@ -3,7 +3,7 @@ export default function () {
 		name: process.env.ENVIRONMENT || 'development',
 		baseURL: process.env.BASE_URL || 'https://ndorfin.github.io/birds-of-zevenwacht/',
 		timestamps: {
-			css: 1734621990411,
+			timestampCSSBuild: 1735225223220,
 			html: 1734619420208,
 			json: 1734619420205,
 			libJS: 1734619420205,
