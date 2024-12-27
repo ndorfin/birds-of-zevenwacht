fb4404e291d15f6111e497b724fc6f4a9878140e
Shaun O'Connell <ndorfin@gmail.com>
2024-12-27 15:48:10 +0200

    Fix map layout

--- src/_data/environment.mjs
+++ src/_data/environment.mjs
@@ -3,7 +3,7 @@ export default function () {
 		name: process.env.ENVIRONMENT || 'development',
 		baseURL: process.env.BASE_URL || 'https://ndorfin.github.io/birds-of-zevenwacht/',
 		timestamps: {
-			css: 1735295856932,
+			css: 1735307275326,
 			html: 1734619420208,
 			json: 1734619420205,
 			libJS: 1734619420205,
