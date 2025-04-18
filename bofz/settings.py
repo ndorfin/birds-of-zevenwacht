e47902bf74473fde6af5863650a1957f41762824
Shaun O'Connell <ndorfin@gmail.com>
2025-04-18 17:24:11 +0200

    Move assets into the static folder and add to settings

--- bofz/settings.py
+++ bofz/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -52,7 +51,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'bofz.urls'
 
 TEMPLATES = [
- {
+  {
     'BACKEND': 'django.template.backends.jinja2.Jinja2',
     'DIRS': [
       os.path.join(BASE_DIR, 'bofz', 'templates', 'bofz')
@@ -123,6 +122,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'bofz/static/'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
