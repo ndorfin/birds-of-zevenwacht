02733d3665e9d6cb5b318d913e4342d906292fda
Shaun O'Connell <ndorfin@gmail.com>
2025-08-06 16:33:59 +0200

    Remove reference to 'Person' model, this is now User

--- scripts/add_fixtures.sh
+++ scripts/add_fixtures.sh
@@ -1,7 +1,6 @@
 #! bin/sh
 # Make sure initial migrations have been made
 python manage.py loaddata ./bofz/fixtures/01_redlist_levels.yaml
-python manage.py loaddata ./bofz/fixtures/02_persons.yaml
 python manage.py loaddata ./bofz/fixtures/03_birds.yaml
 python manage.py loaddata ./bofz/fixtures/04_species_lists_01_SABAP2.yaml
 python manage.py loaddata ./bofz/fixtures/04_species_lists_02_Louis_lifestyle_list.yaml
