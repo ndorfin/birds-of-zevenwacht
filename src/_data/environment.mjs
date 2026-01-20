bf6cab5b0692e7e21cecaa2bac060e592fe30d17
Shaun O'Connell <ndorfin@gmail.com>
2026-01-20 13:55:32 +0200

    Build CSS

--- src/_data/environment.mjs
+++ src/_data/environment.mjs
@@ -3,7 +3,7 @@ export default function () {
 		name: process.env.ENVIRONMENT || 'development',
 		baseURL: process.env.BASE_URL || 'https://ndorfin.github.io/birds-of-zevenwacht/',
 		timestamps: {
-			css: 1761144251869,
+			css: 1768910075388,
 			html: 1734619420208,
 			json: 1734619420205,
 			libJS: 1734619420205,
