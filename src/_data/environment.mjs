d060c8514b2ced5aa0bb6fdb92b9bd2b8525726f
Shaun O'Connell <ndorfin@gmail.com>
2026-05-06 15:50:08 +0200

    Move People + Areas into the main nav

--- src/_data/environment.mjs
+++ src/_data/environment.mjs
@@ -3,7 +3,7 @@ export default function () {
 		name: process.env.ENVIRONMENT || 'development',
 		baseURL: process.env.BASE_URL || 'https://ndorfin.github.io/birds-of-zevenwacht/',
 		timestamps: {
-			css: 1777997081329,
+			css: 1778075388782,
 			html: 1734619420208,
 			json: 1734619420205,
 			libJS: 1734619420205,
